573a535dff47dc658b2beb03249e6ee241d43427
João Barbosa <joao.bffff@gmail.com>
2025-01-31 16:37:59 -0300

    Exemplo da interface comparable

---
+++ InterfaceComparable/src/aplicacao/ProgramaPrincipal.java
@@ -0,0 +1,36 @@
+package aplicacao;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import entidades.Funcionario;
+
+public class ProgramaPrincipal {
+	public static void main(String[] args) {
+		
+		List<Funcionario> list = new ArrayList<>();
+		String path = "C:\\temp\\in.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String funcionarioCsv = br.readLine();
+			
+			while (funcionarioCsv != null) {
+				String[] campos = funcionarioCsv.split(",");
+				list.add(new Funcionario(campos[0], Double.parseDouble(campos[1])));
+				funcionarioCsv = br.readLine();
+			}
+			
+			Collections.sort(list);
+			
+			for (Funcionario f : list) {
+				System.out.println(f.getNome() + "," + f.getSalario());
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
